adc3ff6e21590ff2a9371408086ebc531bdf5371
waadq12-ai <waad.q.12@gmail.com>
2026-06-26 20:10:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("mySidebar");
+    if (sidebar.style.display === "block") {
+        sidebar.style.display = "none";
+    } else {
+        sidebar.style.display = "block";
+    }
+}
 
