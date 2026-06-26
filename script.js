f0d469983dc8140f67d9888e7a6356d00a818d12
waadq12-ai <waad.q.12@gmail.com>
2026-06-26 20:24:55 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleSidebar() {
-    var sidebar = document.getElementById("mySidebar");
-    if (sidebar.style.display === "block") {
-        sidebar.style.display = "none";
-    } else {
-        sidebar.style.display = "block";
-    }
-}
-
